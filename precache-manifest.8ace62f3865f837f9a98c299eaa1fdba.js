e6c0322545b042c79043a12273a3e28fc7cf821d
Rohith889 <rohithreddy889@gmail.com>
2020-06-07 21:32:57 +0530

    Updates

---
+++ precache-manifest.8ace62f3865f837f9a98c299eaa1fdba.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63721e6635214d23513af4c8fbb0291b",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "44a831361c40d2678f77",
+    "url": "/Portfolio/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "4d5eb352e4a2ef50d5dd",
+    "url": "/Portfolio/static/css/main.10f9f015.chunk.css"
+  },
+  {
+    "revision": "44a831361c40d2678f77",
+    "url": "/Portfolio/static/js/2.11960a84.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Portfolio/static/js/2.11960a84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d5eb352e4a2ef50d5dd",
+    "url": "/Portfolio/static/js/main.ffc2f13c.chunk.js"
+  },
+  {
+    "revision": "425e952245b014fc3ffb",
+    "url": "/Portfolio/static/js/runtime-main.db605a1d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
